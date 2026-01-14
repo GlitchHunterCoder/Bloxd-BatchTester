f67c1d2f234c9663380b8dab4516b8aad751738c
GlitchHunterCoder <TheoChalmers2008@gmail.com>
2026-01-14 14:14:02 +0000

    Create Temporal.js

---
+++ modules/Temporal.js
@@ -0,0 +1,16 @@
+[ //temporal count
+    (()=>{
+      globalThis.temporal_unit=0
+      while(true){}
+    }),(()=>{
+      while(1){testF();temporal_unit++;}
+      results[key].IU
+    }),(()=>{
+      let key = tags[test[0]]||test[0]
+      results[key] ??= {}
+      results[key].TU = temporal_unit
+    }),
+    (()=>{
+      delete globalThis.temporal_unit
+    })
+  ]
