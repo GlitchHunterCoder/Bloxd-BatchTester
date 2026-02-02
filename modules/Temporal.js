566b4943d8cab401d32502c4d25ffadaea51462e
GlitchHunterCoder <209635473+GlitchHunterCoder@users.noreply.github.com>
2026-02-02 20:31:57 +0000

    Update Temporal.js

--- modules/Temporal.js
+++ modules/Temporal.js
@@ -1,16 +1,11 @@
 [
-    (()=>{
-      globalThis.temporal_unit=0
-      while(true){}
-    }),(()=>{
-      while(1){testF();temporal_unit++;}
-      results[key].IU
-    }),(()=>{
-      let key = tags[test[0]]||test[0]
-      results[key] ??= {}
-      results[key].TU = temporal_unit
-    }),
-    (()=>{
-      delete globalThis.temporal_unit
-    })
-  ]
+  ()=>{
+    i=0
+    testF() 
+    while(1){eval();i++;}
+  },()=>{
+    addResult("TU",(1/i)-BASE.TU,1/i)
+  },()=>{
+    delete globalThis.i
+  }
+]
